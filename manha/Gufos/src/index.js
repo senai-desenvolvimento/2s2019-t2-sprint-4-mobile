1f5bc02053ff7549bbf337258ebe783db354f18e
unknown <admin.local@TLAB081.info.local>
2019-10-30 10:18:21 -0300

    up

--- manha/Gufos/src/index.js
+++ manha/Gufos/src/index.js
@@ -1,9 +1,15 @@
-import {createAppContainer} from 'react-navigation';
-
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import MainScreen from './pages/main';
 import ProfileScreen from './pages/profile';
+import SignInScreen from './pages/signin';
+
+// criar a navegacao com o login - autenticacao
+const AuthStack = createStackNavigator({
+  Sign: {screen: SignInScreen},
+});
 
 const MainNavigator = createBottomTabNavigator({
   Main: {
@@ -14,4 +20,14 @@ const MainNavigator = createBottomTabNavigator({
   },
 });
 
-export default createAppContainer(MainNavigator);
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      MainNavigator,
+      AuthStack,
+    },
+    {
+      initialRouteName: 'AuthStack',
+    },
+  ),
+);
--- manha/Gufos/src/pages/profile.js
+++ manha/Gufos/src/pages/profile.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, AsyncStorage, View} from 'react-native';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+    };
+  }
+
+  // quando eu abrir a tela de perfil, eu quero buscar os dados do asyncstorage
+  componentDidMount() {
+    this._buscarDadosDoStorage();
+  }
+
+  _buscarDadosDoStorage = async () => {
+    try {
+      const tokenDoStorage = await AsyncStorage.getItem('@gufos:token');
+      if (tokenDoStorage != null) {
+        this.setState({token: tokenDoStorage});
+      }
+    } catch (error) {}
+  };
+
   render() {
-    return <Text>Perfil</Text>;
+    return (
+      <View>
+        <Text>{this.state.token}</Text>
+      </View>
+    );
   }
 }
 
